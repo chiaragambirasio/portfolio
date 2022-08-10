f769b4c19a7e429b9332ad0aa6dda7ff8b4852d1
chiaragambirasio <79317690+chiaragambirasio@users.noreply.github.com>
2022-08-10 02:09:19 +0200

    modify the design and make it more user friendly

--- myJavascript.js
+++ myJavascript.js
@@ -5,166 +5,137 @@ const UXD = document.getElementById("UXD");
 const user = document.getElementById("UserBox");
 const experience = document.getElementById("ExperienceBox");
 const design = document.getElementById("DesignBox");
-const arrowLeft = document.getElementById("arrowLeft");
-const arrowDown = document.getElementById("arrowDown");
-const arrowRight = document.getElementById("arrowRight");
-const U3 = document.getElementById("U3");
 const X3 = document.getElementById("X3");
 const D3 = document.getElementById("D3");
 const AboutMe = document.getElementById("AboutMe");
 const Contact = document.getElementById("Contact");
 const AboutMeBackground = document.getElementById("AboutMeBackground");
+const ContactBackground = document.getElementById("ContactBackground");
 const closeButton = document.getElementById("close");
 const closeButton2 = document.getElementById("close2");
+const sidebar = document.getElementById("sidebar");
+const sidebarbg = document.getElementById("sidebarbg");
 
 // Visibilty
-user.style.display = "none";
-arrowLeft.style.display = "none";
-experience.style.display = "none";
-arrowDown.style.display = "none";
-design.style.display = "none";
-arrowRight.style.display = "none";
+user.style.display = "block";
+experience.style.display = "block";
+design.style.display = " block";
 AboutMeBackground.style.display = "none";
 ContactBackground.style.display = "none";
 
+// buttons landing page
+const personas = document.getElementById("personas");
+personas.onmouseover = personas.onmouseout = UserAction;
+const researchAndTesting = document.getElementById("researchAndTesting");
+researchAndTesting.onmouseover = researchAndTesting.onmouseout = UserAction;
+
+const seness = document.getElementById("seness");
+seness.onmouseover = seness.onmouseout = ExperienceAction;
+const empoweringWomen = document.getElementById("empoweringWomen");
+empoweringWomen.onmouseover = empoweringWomen.onmouseout = ExperienceAction;
+
+const logo = document.getElementById("logo");
+logo.onmouseover = logo.onmouseout = DesignAction;
+const poster = document.getElementById("poster");
+poster.onmouseover = poster.onmouseout = DesignAction;
+
+// Hover U
+
+function UserAction(event) {
+
+  if (event.type == 'mouseover') {
+    document.getElementById("U1").style.fill = "#FFC94B";
+    document.getElementById("U2").style.fill = "#FFC94B";
+    U3.style.fill = "#FFC94B";
+    document.body.style.cursor = "pointer";
+  }
+  if (event.type == 'mouseout') {
+    document.getElementById("U1").style.fill = "#4A6163";
+    document.getElementById("U2").style.fill = "#4A6163";
+    U3.style.fill = "#4A6163";
+    document.body.style.cursor = "default";
+  }
+}
+
+// Hover X
+
+function ExperienceAction(event) {
+
+  if (event.type == 'mouseover') {
+    document.getElementById("X1").style.fill = "#F9A66C";
+    document.getElementById("X2").style.fill = "#F9A66C";
+    X3.style.fill = "#F9A66C";
+    document.body.style.cursor = "pointer";
+  }
+  if (event.type == 'mouseout') {
+    document.getElementById("X1").style.fill = "#4A6163";
+    document.getElementById("X2").style.fill = "#4A6163";
+    X3.style.fill = "#4A6163";
+    document.body.style.cursor = "default";
+  }
+}
+
+// Hover D
+
+function DesignAction(event) {
+
+  if (event.type == 'mouseover') {
+    document.getElementById("D1").style.fill = "#F17A7E";
+  document.getElementById("D2").style.fill = "#F17A7E";
+  D3.style.fill = "#F17A7E";
+  document.body.style.cursor = "pointer";
+  }
+  if (event.type == 'mouseout') {
+    document.getElementById("D1").style.fill = "#4A6163";
+    document.getElementById("D2").style.fill = "#4A6163";
+    D3.style.fill = "#4A6163";
+    document.body.style.cursor = "default";
+  }
+}
+
 // About Me Card
+AboutMe.onmouseover = function (){
+  AboutMe.style.fontWeight = "bold";
+}
+
+AboutMe.onmouseout = function (){
+  AboutMe.style.fontWeight = "normal";
+}
 
 AboutMe.onmousedown = function () {
   AboutMeBackground.style.display = "flex";
+  AboutMe.style.fontWeight = "bold";
   ContactBackground.style.display = "none";
-  user.style.display = "none";
-  experience.style.display = "none";
-  design.style.display = "none";
+  Contact.style.fontWeight = "normal";
+  sidebarbg.style.width = "100vw";
 }
 
-closeButton.onmousedown = function (){
+closeButton.onmousedown = function () {
   AboutMeBackground.style.display = "none";
+  AboutMe.style.fontWeight = "normal";
+  sidebarbg.style.width = "68px";
+}
+
+// Contact Card
+
+Contact.onmouseover = function (){
+  Contact.style.fontWeight = "bold";
+}
+
+Contact.onmouseout = function (){
+  Contact.style.fontWeight = "normal";
 }
 
 Contact.onmousedown = function () {
   AboutMeBackground.style.display = "none";
+  AboutMe.style.fontWeight = "normal";
   ContactBackground.style.display = "flex";
-  user.style.display = "none";
-  experience.style.display = "none";
-  design.style.display = "none";
+  Contact.style.fontWeight = "bold";
+  sidebarbg.style.width = "100vw";
 }
 
-closeButton2.onmousedown = function (){
+closeButton2.onmousedown = function () {
   ContactBackground.style.display = "none";
+  Contact.style.fontWeight = "normal";
+  sidebarbg.style.width = "68px";
 }
-
-
-
-// Hover U
-
-U3.onmouseover = function () {
-
-  document.getElementById("U1").style.fill = "#FFD84C";
-  document.getElementById("U2").style.fill = "#FFD84C";
-  U3.style.fill = "#FFD84C";
-  document.body.style.cursor = "pointer";
-
-  // show relevant boxes
-  user.style.display = "block";
-  arrowLeft.style.display = "block";
-  // hide other boxes
-  experience.style.display = "none";
-  arrowDown.style.display = "none";
-  design.style.display = "none";
-  arrowRight.style.display = "none";
-
-}
-
-U3.onmouseleave = function () {
-
-  document.getElementById("U1").style.fill = "#000";
-  document.getElementById("U2").style.fill = "#000";
-  U3.style.fill = "#000";
-  document.body.style.cursor = "default";
-
-}
-
-// Hover X
-X3.onmouseover = function () {
-
-  document.getElementById("X1").style.fill = "#D891F9";
-  document.getElementById("X2").style.fill = "#D891F9";
-  X3.style.fill = "#D891F9";
-  document.body.style.cursor = "pointer";
-
-  // show relevant boxes
-  experience.style.display = "block";
-  arrowDown.style.display = "block";
-  // hide other boxes
-  user.style.display = "none";
-  arrowLeft.style.display = "none";
-  design.style.display = "none";
-  arrowRight.style.display = "none";
-}
-
-X3.onmouseleave = function () {
-
-  document.getElementById("X1").style.fill = "#000";
-  document.getElementById("X2").style.fill = "#000";
-  X3.style.fill = "#000";
-  document.body.style.cursor = "default";
-
-}
-
-// Hover D
-D3.onmouseover = function () {
-
-  document.getElementById("D1").style.fill = "#A9CDDD";
-  document.getElementById("D2").style.fill = "#A9CDDD";
-  D3.style.fill = "#A9CDDD";
-  document.body.style.cursor = "pointer";
-
-  // show relevant boxes
-  design.style.display = "block";
-  arrowRight.style.display = "block";
-  // hide other boxes
-  user.style.display = "none";
-  arrowLeft.style.display = "none";
-  experience.style.display = "none";
-  arrowDown.style.display = "none";
-
-}
-
-D3.onmouseleave = function () {
-
-  document.getElementById("D1").style.fill = "#000";
-  document.getElementById("D2").style.fill = "#000";
-  D3.style.fill = "#000";
-  document.body.style.cursor = "default";
-
-}
-
-// holographic backgorund movement
-
-function updateHolographicBackground(value) {
-  const percentage = value * 100;
-  AboutMeBackground.style.backgroundPosition = percentage + "%";
-  ContactBackground.style.backgroundPosition = percentage + "%";
-}
-
-function handleMouseMove(event) {
-  const x = event.clientX;
-  const width = document.documentElement.clientWidth;
-  const value = x / width;
-  updateHolographicBackground(value);
-}
-
-function handleDeviceOrientation(event) {
-  const z = Math.abs(event.alpha); // rotation degrees from 0 to 360
-  const value = z / 360;
-  updateHolographicBackground(value);
-}
-
-window.addEventListener("deviceorientation", handleDeviceOrientation, true);
-
-
-// circle thing
-
-// creates a span wrapping each letter of text
-// Appends spans to target element
-// Adds rotate transform to each letter
